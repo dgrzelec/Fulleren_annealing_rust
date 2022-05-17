e9202533d8b0d1a35245db072f45c91f46ed53e2
Daniel Grzelec <danielgrzelec@gmail.com>
2022-05-17 13:37:15 +0200

    add test data + randomize and assert angles methods

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{io::{Write}, collections::{VecDeque, HashSet}, ops::Index};
-use ndarray::{prelude::*, IndexLonger};
+use std::{io::{Write}, collections::{VecDeque, HashSet}, ops::Index, f64::consts::PI};
+use ndarray::{prelude::*, IndexLonger, AssignElem};
 use rand::prelude::*;
 use utilities::save_gnuplot2D;
 
@@ -19,7 +19,6 @@ const a0: f64 = 0.011304;
 const c0: f64 = 19.;
 const d0: f64 = 2.5;
 // ##############################
-const N: usize = 10+2;
 type MatrixInt = Array2<i32>;
 type VectorInt = Array1<i32>;
 type VectorFloat = Array1<f64>;
@@ -37,8 +36,7 @@ struct Point6 {
 
 impl Point6 {
     fn new() -> Point6 {
-        // Point6 {}
-        todo!()
+        Point6 {x: 0., y: 0., z: 0., r: 0., phi: 0., theta: 0.}
     }
 
     fn from_cartesian<T: Index<usize, Output = f64>>(data: &T) -> Point6 {
@@ -53,23 +51,83 @@ impl Point6 {
                  phi: (yt/xt).atan(), 
                  theta: (zt/rt).acos() }
     }
+
+    fn from_spherical<T: Index<usize, Output = f64>>(data: &T) -> Point6 {
+        let r = data[0];
+        let phi = data[1];
+        let theta = data[2];
+
+        Point6 { x: r*theta.sin()*phi.cos(), 
+                 y: r*theta.sin()*phi.sin(), 
+                 z: r*theta.cos(), 
+                 r, 
+                 phi, 
+                 theta }
+    }
+    // methods
+
+    fn assert_angles(&mut self) {
+        //phi [0, 2*PI]
+        if self.phi < 0. { self.phi += 2.*PI}
+        else if self.phi >2.*PI { self.phi -= 2.*PI  }
+
+        //theta [0, PI]
+        if self.theta < 0. { self.theta += PI}
+        else if self.theta > PI { self.theta -= PI  }
+
+    }
 }
+
+type Point6Array = Array1<Point6>;
+
 #[derive( Debug)]
 struct Fuleren {
-    positions: Array1<Point6>,
-
+    positions: Point6Array,
+    size: usize,
+    E: f64,
 }
 
 impl Fuleren {
-    fn new() -> Fuleren {
-        todo!()
+    // constructors
+    fn new(size: usize) -> Fuleren {
+        Fuleren { positions: Point6Array::from_elem(size, Point6::new()),
+                  size,
+                  E: 0. }
     }
     
+    fn from_file(size: usize, path: &str) -> Fuleren {
+        todo!()
+    }
+
+    // methods
+    fn randomize_on_sphere(&mut self, r: f64) {
+        let phi_distr = rand::distributions::Uniform::new_inclusive(0., 2.*PI);
+        let theta_distr = rand::distributions::Uniform::new_inclusive(0., PI);
+        let mut rng = rand::thread_rng();
+
+        self.positions.iter_mut()
+                      .for_each(|point| 
+                                point.assign_elem(Point6::from_spherical(&[r, 
+                                                                            rng.sample(phi_distr), 
+                                                                            rng.sample(theta_distr)]) ));
+    }
 }
 
 // ####################################
 // ########### functions #############
 
+
+fn check_angles(mut phi: f64, mut theta: f64) -> (f64, f64) {
+    //phi [0, 2*PI]
+    if phi < 0. { phi += 2.*PI}
+    else if phi >2.*PI { phi -= 2.*PI  }
+
+    //theta [0, PI]
+    if theta < 0. { theta += PI}
+    else if theta > PI { theta -= PI  }
+
+    (phi, theta)
+}
 
 
 // ##################################
@@ -78,11 +136,18 @@ fn main() {
     let mut rng = rand::thread_rng();
     // let distr = rand::distributions::Uniform::new_inclusive(0, 3);
 
-    
+    let N = 10;
     let mut Board = MatrixInt::from_elem((N,N), 5);
 
     let point1 = Point6::from_cartesian(&[1.,2.,3.]);
 
-    println!("{:?}", point1);
+    let mut F = Fuleren::new(10);
+
+    println!("{:?}", F.positions);
+
+    F.randomize_on_sphere(1.);
+    println!("{:?}", F.positions);
+
+    
 }
 
